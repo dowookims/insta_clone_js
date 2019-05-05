e891fb157774125f4122e107e52bcc9c9c5d4993
dowookims <kaei2574@gmail.com>
2019-05-06 01:02:41 +0900

    Add some data model in datamodel.prisma and Update me.js => fullName attribute using Prisma parameter

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -10,8 +10,17 @@ export default {
       const userProfile = await prisma.user({ id: user.id});
       const posts = await prisma.user({ id: user.id}).posts();
       return {
-        userProfile, posts
+        user: userProfile, 
+        posts
       };
+    }
+  },
+  User: {
+    // fullName: (_, __, { request })
+    // @param 1 is a parent(root) resolver that above the query. if i called fullName, parent will be user
+    
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`
     }
   }
 };
